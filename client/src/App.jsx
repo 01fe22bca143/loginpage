34861ac1b0294fb80ea3b17a5caea3f5fbdf4b7f
01fe22bca143 <01fe22bca143@kletech.ac.in>
2024-03-05 21:27:03 +0530

    code for login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@
  import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Login from './Login';
 import Signup from './Signup';
-import Home from './Home';
 
 
 
@@ -14,7 +13,7 @@ function App() {
    <Routes>
     <Route path='/register' element={<Signup />}></Route>
     <Route path='/login' element={<Login />}></Route>
-    <Route path='/home' element={<Home />}></Route>
+   
 
 
 
